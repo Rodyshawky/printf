1e7c06fac9fbe46f7482443ca36d6e550c98e4d7
ahmed mohamed gomaa <ahmedmgomaa01@gmail.com>
2023-09-19 17:24:09 -0700

    h

--- main.h
+++ main.h
@@ -10,5 +10,14 @@
 int print_char(char c);
 int print_N(char *str);
 int _printf(const char *format, ...);
-
+void printIntWithFieldWidth;
+void printDoubleWithFieldWidth;
+int main();
+int _printf(const char *format, ...);
+int _putchar(char c);
+char data[BUFFER_SIZE];
+int my_printf(const char *format, ...);
+int custom_printf(const char *format, ...);
+int print_binary(uint32_t num);
+int print_binary(uint32_t num);
 #endif
