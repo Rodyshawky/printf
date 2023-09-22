5a2fb112da46c0b772b3389f5a66948f202a1488
rodyshawky <rodashawkei@gmail.com>
2023-09-22 14:43:50 -0700

    task 0

--- main.c
+++ main.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int c = 'o';
 	char *s = "Hello";
-	_printf("%c", c);
+
+	_printf("%c\n", c);
 	_printf("%!");
-	_printf("%s", s);
+	_printf("%s\n", s);
 	_printf("Let's print a simple sentence.\n");
 	_printf("%K\n");
 	_printf("%c", '\0');
 	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
-	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
 	return (0);
 }
