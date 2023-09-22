e2936bba3dbc41dff082af2a304e4a1d9d3836b2
rodyshawky <rodashawkei@gmail.com>
2023-09-22 13:58:34 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -10,45 +10,43 @@
  */
 int _printf(const char *format, ...)
 {
-	
-        char *str;
-        int len = 0;
-        va_list arg;
-        int count = 0;
-        char c;
+char *str;
+int len = 0;
+va_list arg;
+int count = 0;
+char c;
 
-        if (format == NULL)
-                return (-1);
-        va_start(arg, format);
-        while (*format)
-        {
-                if (*format != '%')
-                {
-                        write(1, format, 1);
-                        count++;
-                }
-                else
-                {
-                        format++;
-                if (*format == '\0')
-                       break;
-		else if (*format == 'c')
-                {
-                        c = va_arg(arg, int);
-                        write(1, &c, 1);
-                        count++;
-                }
-		else if (*format == 's')
-                {
-                        str = va_arg(arg, char*);
-			while (str[len] != '\0')
-				len++;
-                        write(1, str, len);
-                        count += len;
-                }
-                }
+if (format == NULL)
+	return (-1);
+va_start(arg, format);
+while (*format)
+{
+	if (*format != '%')
+	{
+		write(1, format, 1);
+		count++;
+	}
+	else
+	{
 		format++;
-        }
-                va_end(arg);
-                return (count);
+		if (*format == '\0')
+			break;
+		else if (*format == 'c')
+		{
+			c = va_arg(arg, int);
+			write(1, &c, 1);
+			count++;
+		}
+		else if (*format == 's')
+		{
+			str = va_arg(arg, char*);
+			len = strlen(str);
+			write(1, str, len);
+			count += len;
+		}
+		}
+		format++;
+}
+		va_end(arg);
+		return (count);
 }
