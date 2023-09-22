7de67016781d9ae302079f70b3590f3fc3b68adb
rodyshawky <rodashawkei@gmail.com>
2023-09-22 13:30:50 -0700

    Task 0

--- main.h
+++ main.h
@@ -4,19 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
 
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-int print_char(char c);
-int get_flag(char s, flags_t *f);
-int (*format_print(char s))(va_list, flags_t);
 int _printf(const char *format, ...);
-int (*pfunc)(va_list, flags_t *);
-int _putchar(char c);
+
 #endif
