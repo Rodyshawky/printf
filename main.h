a688d8794d1a94295143a08c959387436ba56bc4
rodyshawky <rodashawkei@gmail.com>
2023-09-26 15:22:15 -0700

    task 0

--- main.h
+++ main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#define BUFFER_SIZE 1024
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-
+char buffer[BUFFER_SIZE];
 #endif
